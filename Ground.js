6005bba6e7b278addb7882da2135a7c0165ef5b4
samy3333 <70003427+samy3333@users.noreply.github.com>
2021-02-25 20:42:18 +1100

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -1,17 +1,33 @@
-class Ground {
-    constructor(x,y,width,height) {
-      var options = {
-          isStatic: true
+//class Ground {
+   // constructor(x,y,width,height) {
+     // var options = {
+          //isStatic: true
+      //}
+      //this.body = Bodies.rectangle(x,y,width,height,options);
+      //this.width = width;
+      //this.height = height;
+     // World.add(world, this.body);
+    //}
+    //display(){
+    //  var pos =this.body.position;
+      //rectMode(CENTER);
+      //fill("yellow");
+      //rect(pos.x, pos.y, this.width, this.height);
+  //  }
+ // }
+
+class Ground{
+     constructor( x, y, width, height){ 
+       var ground_options={
+         isStatic : true 
       }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
-      World.add(world, this.body);
+      this.ground = Bodies.rectangle(450,390,900,20,ground_options)
+      World.add(world,this.ground);
     }
     display(){
-      var pos =this.body.position;
-      rectMode(CENTER);
-      fill("yellow");
-      rect(pos.x, pos.y, this.width, this.height);
-    }
+       noStroke(); 
+       fill(188,67,67);
+       rectMode(CENTER); 
+       rect(this.ground.position.x,this.ground.position.y,900,20);
   }
+}
